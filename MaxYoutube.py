9e41bd877ebcf5a45cceb6f6b57ee061139d00f4
Rafael_Guedes <rafatattoox@gmail.com>
2023-02-26 20:54:43 -0300

    Fix_Bugs_One

--- MaxYoutube.py
+++ MaxYoutube.py
@@ -1,16 +1,14 @@
-
 # Rafael guedes
 # github.com/guedes2142
-
 from pytube import YouTube
 from time import sleep
-import time
 from rich.progress import track
 import os
-from pytube import Search
 from colorama import Fore, Style
 import pygame
 from pygame.locals import *
+from musictheme import *
+import pyttsx3
 # colors
 RED = "\033[1;31m"
 BLUE = "\033[1;34m"
@@ -19,18 +17,25 @@ GREEN = "\033[0;32m"
 RESET = "\033[0;0m"
 BOLD = "\033[;1m"
 REVERSE = "\033[;7m"
+
+engine = pyttsx3.init()
+
+
+
+#Background music
+playsound()
 # path
 # SAVE_PATH = 'C:/Users/Rafael/Desktop'
 # banner
-text = 'thanks for use me'
-print(CYAN + f'[{text:*^88}]' + RESET)
+text = 'Thanks for use'
+print(CYAN + f'{text:*^88}' + RESET)
+print()
 print(RED + '███╗░░░███╗░█████╗░██╗░░██╗   ██╗░░░██╗░█████╗░██╗░░░██╗████████╗██╗░░░██╗██████╗░███████╗' + RESET)
 print(RED + '████╗░████║██╔══██╗╚██╗██╔╝  ╚██╗░██╔╝██╔══██╗██║░░░██║╚══██╔══╝██║░░░██║██╔══██╗██╔════╝' + RESET)
 print(RED + '██╔████╔██║███████║░╚███╔╝░  ░╚████╔╝░██║░░██║██║░░░██║░░░██║░░░██║░░░██║██████╦╝█████╗░░' + RESET)
 print(RED + '██║╚██╔╝██║██╔══██║░██╔██╗░  ░░╚██╔╝░░██║░░██║██║░░░██║░░░██║░░░██║░░░██║██╔══██╗██╔══╝░░' + RESET)
 print(RED + '██║░╚═╝░██║██║░░██║██╔╝╚██╗  ░░░██║░░░╚█████╔╝╚██████╔╝░░░██║░░░╚██████╔╝██████╦╝███████╗' + RESET)
 print(RED + '╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝  ░░░╚═╝░░░░╚════╝░░╚═════╝░░░░╚═╝░░░░╚═════╝░╚═════╝░╚══════╝' + RESET)
-
 print(GREEN + '██████╗░░█████╗░███╗░░██╗░██╗░░░░░░░██╗██╗░░░░░░█████╗░░█████╗░██████╗░███████╗██████╗░' + RESET)
 print(GREEN + '██╔══██╗██╔══██╗████╗░██║░██║░░██╗░░██║██║░░░░░██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗' + RESET)
 print(GREEN + '██║░░██║██║░░██║██╔██╗██║░╚██╗████╗██╔╝██║░░░░░██║░░██║███████║██║░░██║█████╗░░██████╔╝' + RESET)
@@ -41,84 +46,91 @@ print(BLUE + 'by.Rafael Guedes' + RESET)
 print(CYAN + 'github.com/guedes2142' + RESET)
 print()
 print(RED + 'Obs: maybe you video dont have some resolutions and this may cause problems' + RESET)
-print(Fore.YELLOW + 'Downloading copyrighted YouTube videos is illegal!\n '
-    'I am not responsible for your downloads! Go at your own risk!' + Style.RESET_ALL)
-
-
-pygame.init()
-musicafundo = pygame.mixer.music.load('123.mp3')
-pygame.mixer.music.play(-1)
-
+print(Fore.YELLOW + 'Downloading copyrighted YouTube videos is illegal!\n'
+'I am not responsible for your downloads! Go at your own risk!' + Style.RESET_ALL)
 
 while True:
-    
-    
-    print(GREEN + 'note:The file will be saved in the same folder as this app ' + RESET)
-    print()
+
+    print(GREEN + 'Note:The file will be saved in the same folder as this app ' + RESET)
+    print(BLUE + '****************************************************************************************' + RESET)
     # options
-    print(RED + 'choose an option' + RESET)
-    print(GREEN + '1: Video download' + RESET)
-    print(BLUE + '2: Video only audio.mp4' + RESET)
-    print(GREEN + '3: Search' + RESET)
-    print(RED + '4: exit' + RESET)
+    print(RED + f'Choose an option' + RESET)
+    print(Fore.GREEN + '1 >> Download video' + Style.RESET_ALL)
+    print(Fore.LIGHTGREEN_EX + '2 >> Only audio.mp4 ' + Style.RESET_ALL)
+    print(Fore.BLUE + '3 >> Search' + Style.RESET_ALL)
+    print(Fore.LIGHTBLUE_EX + '4 >> exit' + Style.RESET_ALL)
+    print(Fore.LIGHTRED_EX + '5 >> Stop music' + Style.RESET_ALL)
     print()
     choice = input('')
-    print()
-    '''
-    if choice =='3': #TODO: ver como pega o resultado e usar as opções para download
-        searchChoice = input('')
-        s = Search('YouTube Rewind')
-        print(s.results)
-        continue
-    '''
 
     if choice == '1':
-        print('Choose a resolution [1080],[720],[360] ')
+        print(Fore.LIGHTCYAN_EX + 'Choose a resolution [1080],[720],[360] ' + Style.RESET_ALL)
         resolution = input()
-        
+
         if resolution == '1080':
             try:
                 print(GREEN + 'Insert the video link' + RESET)
-                videoLink = str(input())
+                videoLink = str(input(''))
                 yt = YouTube(f'{videoLink}')
+                print('You are downloading>> ',yt.title5)
                 for i in track(range(10), description="Downloading..."):
-                    time.sleep(1)  # Simulate work being done
-                stream = yt.streams.get_by_itag(137)
-                stream.download()
+                    sleep(1)  # Simulate work being done
+                    stream = yt.streams.get_by_itag(137)
+                    stream.download()
             except:
-                print('Conexion error,or please try 720 or 360 px')
+                print('Error! checkout you conection,or please try 720 360 px')
+                print('Maybe you video dont have some resolutions')
+                
                 break
             print(GREEN + 'Success' + RESET)
+            leia = engine.say('Download from youtube was completed successfully')
+            engine.runAndWait()
             continue
 
         elif resolution == '720':
+            
             try:
                 print(GREEN + 'Insert the video link' + RESET)
-                videoLink = str(input())
+                videoLink = str(input(''))
+                
                 yt = YouTube(f'{videoLink}')
+                
+                print('You are downloading>> ',yt.title5)
                 for i in track(range(10), description="Downloading..."):
-                    time.sleep(1)  # Simulate work being done
-                stream = yt.streams.get_by_itag(22)
-                stream.download()
+                    sleep(1) 
+                    print('Please wait===================*')
+                    
+                    stream = yt.streams.get_by_itag(22)
+                    stream.download()
             except:
-                print('Conexion error')
+                print(Fore.YELLOW + 'Error checkout you conection,or please try 360 px' )
+                print('Maybe you video dont have some resolutions')
+                print("if don't work yet please contact me, or made me pull request on GitHub" + Style.RESET_ALL)
                 break
             print(GREEN + 'Success' + RESET)
+            leia = engine.say('Download from youtube was completed successfully')
+            engine.runAndWait()
             continue
 
         elif resolution == '360':
             try:
                 print(GREEN + 'Insert the video link' + RESET)
-                videoLink = str(input())
+                videoLink = str(input(''))
                 yt = YouTube(f'{videoLink}')
+                print('You are downloading>> ',yt.title5)
                 for i in track(range(10), description="Downloading..."):
-                    time.sleep(1)  # Simulate work being done
-                stream = yt.streams.get_by_itag(22)
-                stream.download()
-            except:
-                print('Conexion error')
-                break
+                    sleep(1)
+                    print('Please wait===================*')
+
+                    stream = yt.streams.get_by_itag(18)
+                    stream.download()
+            except Exception() as e:
+                print('The following erro happend',e)
+                print("if don't work yet please contact me, or made me pull request on GitHub" + Style.RESET_ALL)
+            
             print(GREEN + 'Success' + RESET)
+            leia = engine.say('Download from youtube was completed successfully')
+            engine.runAndWait()
             continue
 
         else:
@@ -132,10 +144,13 @@ while True:
 
     elif choice == '2':
         print('Insert the video link')
-        videoLink = str(input())
+        videoLink = str(input(''))
         yt = YouTube(f'{videoLink}')
         ys = yt.streams.filter(only_audio=True).first()
         ys.download()
+        
+    elif choice == '5':
+        stopSong()
 
     else:
         choice == '4' or choice == 'exit'
